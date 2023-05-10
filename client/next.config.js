841b8617f0fedd91c52f23813d3f71c8446cc324
upendra <upendra@upendras-MacBook-Air.local>
2023-05-11 03:34:40 +0530

    modified nextConfigJson

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/client/pages/index.tsx",
+      },
+    ];
+  },
+};
